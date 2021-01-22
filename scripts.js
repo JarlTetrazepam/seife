18d79672b07fe5361203fd181ab330e7251e9823
JarlTetrazepam <gaterzoom3638@gmail.com>
2021-01-22 09:55:41 +0100

    added h3 scaling, imnproved footer font size

--- scripts.js
+++ scripts.js
@@ -20,11 +20,17 @@ var x = bodyWidth;
 var perfectPresItemSize = 1.3116 * Math.pow(10, -4) * Math.pow(x, 2) + 1.50607148 * x + 10.3241424; //formula acquired through testing
 var perfectFontSize = 1.5007 * Math.pow(10, -12) * Math.pow(x, 4) - 9.651 * Math.pow(10, -9) * Math.pow(x, 3) + 1.948 * Math.pow(10, -5) * Math.pow(x, 2) - 0.0109467 * x + 17.9363218 //formula acquired through testing
 var perfectNavFontSize = 27.1920288 / (1 + 3.93781513 * Math.exp((-1.7729 * Math.pow(10, -3) * x))) // "
+var perfectHeadingSize = 140.392202 / (1 + 5.72799519 * Math.exp(-3.726 * Math.pow(10, -4) * x))
 
 window.onload = function() {dynamicScaling(bodyWidth, presItem)};
 window.onresize = function() {dynamicScaling(bodyWidth, presItem)};
 
 function dynamicScaling(width, presItem) {
+  let presItemHeading = document.getElementsByClassName("presItemHeading")
+  for (let i = 0; i < presItemHeading.length; i++) {
+      presItemHeading[i].style.fontSize = perfectHeadingSize + "px";
+  }
+
   document.body.style.fontSize = perfectFontSize + "px";
 
   if (bodyWidth < 1280) {
